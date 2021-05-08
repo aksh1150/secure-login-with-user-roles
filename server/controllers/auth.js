a610527c60a7b3ad6b1b1cedd81ed2fac01ac2cc
Akshit Patel <aksh1150@gmail.com>
2021-05-07 20:30:31 -0400

    Add validations

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -4,7 +4,12 @@ import { hashPassword, comparePassword  } from '../utils/auth';
 
 export const register = async (req, res) => {
    try {
-    console.log(req.body)
+    const {name, email, password} = req.body
+
+    // validation
+    if(!name) return res.status(400).send("Name is required!")
+    if(!password || password.length < 6) return res.status(400).send("Password is required and should be six characters long!")
+    if(!email) return res.status(400).send("Email is required!")
    } catch (err) {
        console.log(err)
        return res.status(400).send('Error Try again.')
