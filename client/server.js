50e0b7ddfc9582677d91fee2adb3eae3b384a964
Akshit Patel <aksh1150@gmail.com>
2021-05-08 11:03:45 -0400

    Create proxy server for dev

---
+++ client/server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const next = require('next')
+const { createProxyMiddleware } = require('http-proxy-middleware')
+
+// check if we are in devlopement or production mode
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+
+app.prepare().then(() => {
+    const server = express()
+    // apply proxy in dev mode
+    if(dev) {
+        server.use('/', createProxyMiddleware({
+            target: 'http://localhost:8000/api/', // this is same url which we define in .env.local
+            changeOrigin: true,
+        }))
+    }
+    server.all('*', (req, res) => {
+        return handle(req, res)
+    });
+
+    server.listen(3000, (err) => {
+        if(err) throw err;
+        console.log('> Ready on http://localhost:8000/api/')
+    })
+})
+.catch((err) => {
+    console.log("Error ", err)
+})
