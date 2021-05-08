71145784cd3529bd60a09f7fe56322d3c772183a
Akshit Patel <aksh1150@gmail.com>
2021-05-08 11:06:19 -0400

    Install express in client

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b394b65671fba8930a63";
+/******/ 	var hotCurrentHash = "0adc4bda680edce93b12";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
