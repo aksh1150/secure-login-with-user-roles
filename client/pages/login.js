5296a5989e4e19cd3cde6da6147066ef7f625dcf
Akshit Patel <aksh1150@gmail.com>
2021-05-09 10:06:23 -0400

    Use context and dispatch login payload

--- client/pages/login.js
+++ client/pages/login.js
@@ -1,14 +1,19 @@
-import {useState} from 'react';
+import { useState, useContext } from 'react';
 import axios from 'axios';
 import { toast } from 'react-toastify'
 import { SyncOutlined } from '@ant-design/icons'
 import Link from 'next/link'
+import { Context } from "../context"
 
 const Login = () => {
-
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [loading, setLoading] = useState(false)
+
+  // state
+const {state, dispatch} = useContext(Context)
+
+
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -18,8 +23,11 @@ const Login = () => {
         email,
         password
       });
-     
-      // setLoading(false);
+     dispatch({
+       type: "LOGIN",
+       payload: data,
+     })
+      setLoading(false);
     } catch (err) {
       toast.error(err.response.data)
       setLoading(false)
