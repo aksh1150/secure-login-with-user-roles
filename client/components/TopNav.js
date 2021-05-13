d80f3306d8a2d5fd5cb6721dfe3b89bc89f59c23
Akshit Patel <aksh1150@gmail.com>
2021-05-13 15:29:27 -0400

    import axios in topnav

--- client/components/TopNav.js
+++ client/components/TopNav.js
@@ -2,7 +2,8 @@ import { useState, useEffect, useContext } from 'react'
 import { Menu } from "antd"
 import Link from "next/link"
 import {AppstoreOutlined, LoginOutlined, UserAddOutlined} from "@ant-design/icons"
-import { Context } from "../context" 
+import { Context } from "../context"
+import axios from "axios"
 
 const { Item } = Menu
 
@@ -14,6 +15,10 @@ const TopNav = () => {
   useEffect(() => {
     process.browser && setCurrent(window.location.pathname)
   }, [process.browser && window.location.pathname])
+
+
+  const logout = async ()
+
   return (
     <Menu mode="horizontal" selectedKeys={[current]}>
       <Item key="/" onClick={(e) => setCurrent(e.key)} icon={<AppstoreOutlined/>}>
