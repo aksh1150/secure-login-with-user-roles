40d981c7b5c63d23d536648a3b474850cb63cbc8
Akshit Patel <aksh1150@gmail.com>
2021-05-13 15:24:22 -0400

    Clear cookie and send signout success messge

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -81,7 +81,8 @@ export const login = async (req, res) => {
 // Logout
 export const logout = async (req, res) => {
     try {
-
+        res.clearCookie('token');
+        return res.json({message: "Signout Success!"})
     } catch (err) {
         console.log(err)
     }
