16069e99fc5d0106a381e78a9d2701c6054df5a6
Akshit Patel <aksh1150@gmail.com>
2021-05-15 10:00:20 -0400

    if user logged in redirect user if they come on register

--- client/pages/register.js
+++ client/pages/register.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import { toast } from 'react-toastify'
 import { SyncOutlined } from '@ant-design/icons'
 import Link from 'next/link'
+import { Context } from "../context"
+import { useRouter } from 'next/router'
 
 const Register = () => {
 
@@ -10,6 +12,17 @@ const Register = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [loading, setLoading] = useState(false)
+
+  // destructure user other way
+const {state: {user}, dispatch} = useContext(Context)
+
+
+const router = useRouter()
+
+
+useEffect(() => {
+  if(user !== null) router.push("/");
+},[user])
 
   const handleSubmit = async (e) => {
     e.preventDefault();
