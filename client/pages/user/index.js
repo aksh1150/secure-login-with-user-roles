966518e3aa29a5dc4972efb320658459641d1e10
Akshit Patel <aksh1150@gmail.com>
2021-05-24 10:05:23 -0400

    Add some hooks and context api to fetch user

--- client/pages/user/index.js
+++ client/pages/user/index.js
@@ -1,4 +1,17 @@
+import { useEffect, useState, useContext } from 'react'
+import { Context } from '../../context'
+import axios from 'axios'
+
 const UserIndex = () => {
+    useEffect(() => {
+        const fetchUser = async () => {
+            try {
+
+            } catch(err) {
+                console.log(err)
+            }
+        }
+    },[])
     return (
         <h1 className="jumbotron text-center square">User</h1>
     )
