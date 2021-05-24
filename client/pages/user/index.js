2e6c6132547f46bba66a0e2ec6360b3c3f0b6148
Akshit Patel <aksh1150@gmail.com>
2021-05-24 10:07:41 -0400

    Get user from context state

--- client/pages/user/index.js
+++ client/pages/user/index.js
@@ -3,17 +3,22 @@ import { Context } from '../../context'
 import axios from 'axios'
 
 const UserIndex = () => {
+    // get user from context state
+    const { state: {user} } = useContext(Context)
     useEffect(() => {
         const fetchUser = async () => {
             try {
-
+                const { data } = await axios.get('/api/current-user');
+                console.log(data);
             } catch(err) {
                 console.log(err)
             }
         }
     },[])
     return (
-        <h1 className="jumbotron text-center square">User</h1>
+        <h1 className="jumbotron text-center square">
+            <pre></pre>
+        </h1>
     )
 }
 
