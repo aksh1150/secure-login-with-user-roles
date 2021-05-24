b17d433d916751d6fa6283b4af94bf6feadcca42
Akshit Patel <aksh1150@gmail.com>
2021-05-24 10:47:10 -0400

    Remove unwanted dublicate code and add userRoute

--- client/pages/user/index.js
+++ client/pages/user/index.js
@@ -1,27 +1,11 @@
-import { useEffect, useState, useContext } from 'react'
+import { useContext } from 'react'
 import { Context } from '../../context'
-import axios from 'axios'
+import UserRoute from '../../components/routes/UserRoute'
 
 const UserIndex = () => {
-    const [hidden, setHidden] = useState(true);
     // get user from context state
     const { state: {user} } = useContext(Context);
 
-     const fetchUser = async () => {
-            try {
-                const { data } = await axios.get('/api/current-user');
-                console.log(data);
-                setHidden(false);
-            } catch(err) {
-                console.log(err);
-                setHidden(true);
-            }
-        };
-
-
-    useEffect(() => {
-        fetchUser();
-    },[]);
 
 
     return (
